b35241cbceea4af8d3c9e02bb04c0cfaffd29d15
Mikhail Chabanov <38302151+f-myself@users.noreply.github.com>
2019-03-26 15:46:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include "config.php";
+include "functions.php";
+
+$statusMessage = NULL;
+$tableStatus = NULL;
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+	if(move_uploaded_file($_FILES['upload']['tmp_name'], DIR_PATH . charsetFileNameToSys(basename($_FILES['upload']['name']))))
+	{
+		$statusMessage = OK_FILE_UPLOAD;
+	} else {
+		$statusMessage = checkErrorUpload($_FILES['upload']['error']);
+	}
+}
+
+getFileList();
+
+
+include "templates/index.php";
