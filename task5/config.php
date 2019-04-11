dd3b17d0dfb42bbca2a8398d5e82bd4513499dd8
f-myself <freaking.yourself@gmail.com>
2019-04-11 18:33:47 +0300

    config

--- task5/config.php
+++ task5/config.php
@@ -4,6 +4,6 @@ define('PATH_INI', 'files/variables.ini');
 define('PATH_JSON', 'files/file.JSON');
 
 define(DB_HOST, 'localhost');
-define(DB_USER, 'root');
-define(DB_PASSWORD, '');
+define(DB_USER, 'user6');
+define(DB_PASSWORD, 'user6');
 define(DB_NAME, 'user6');
